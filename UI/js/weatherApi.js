a639c4f2562964038eecacd74cab40c59da090b7
Aaron <aaronrobinett@gmail.com>
2021-09-16 18:50:33 -0700

    created getForecast function

--- UI/js/weatherApi.js
+++ UI/js/weatherApi.js
@@ -1,24 +1,29 @@
 var apiKey = "rwwHjEYvXSeymqIdMcbTbUALxuTpivIX";
-var getLocationKey = function(location) {
-    var city = "portland";
-    var state = "or"
-    var apiUrl = "http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=" + apiKey + "&q=" + city +"%2C%20" + state;
+var getLocationKey = function (location) {
+    var city = location.trim().split(",")[0].toLowerCase();
+    var state = location.trim().split(" ")[1].toLowerCase();
+    var apiUrl = "http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=" + apiKey + "&q=" + city + "%2C%20" + state;
 
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 locationKey = data[0].Key;
-                console.log(locationKey);
+                getForecast(locationKey);
             });
         } else {
             //remove this later /////////
             alert("error: no response for city")
         }
     })
-    .catch(function (error) {
-        /// remove this later /////////
-        alert("unable to connect to accuweather")
-    })
+        .catch(function (error) {
+            /// remove this later /////////
+            alert("unable to connect to accuweather")
+        })
+}
+
+var getForecast = function (key) {
+    console.log(key);
 }
 
 getLocationKey("Portland, OR");
+
